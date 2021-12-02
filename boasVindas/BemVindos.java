a49f1998c3f1adfa9776d6d511b7a41205848220
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2021-12-02 18:10:47 -0300

    mensagem de boas vindas

--- boasVindas/BemVindos.java
+++ boasVindas/BemVindos.java
@@ -4,7 +4,8 @@ public class BemVindos {
 
     public void welcome(){
         System.out.println("------------------------------------------------------------");
-        System.out.println("Bem vindos a Academia do Biel!!");
+        System.out.println("Bem vindos a Academia");
+        System.out.println("Participantes: Gabriel Alves, Angelo Victor, Thierry Silva, Flavio Moreira");
         System.out.println("Por favor siga as instruções para um melhor atendimento...");
         System.out.println("------------------------------------------------------------");
 
